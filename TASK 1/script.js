7579c08d28d5343357e4ef5d2ad845c6e94dc1ae
salonibasare06 <121510126+salonibasare06@users.noreply.github.com>
2023-07-18 18:57:49 +0530

    Add files via upload

---
+++ TASK 1/script.js
@@ -0,0 +1,58 @@
+const fromText = document.querySelector(".from-text"),
+toText = document.querySelector(".to-text"),
+exchageIcon = document.querySelector(".exchange"),
+selectTag = document.querySelectorAll("select"),
+icons = document.querySelectorAll(".row i");
+translateBtn = document.querySelector("button"),
+
+selectTag.forEach((tag, id) => {
+    for(const country_code in country){
+        let selected;
+        if(id==0 && country_code == "en-IN"){
+        }else if (id==1 && country_code == "mr-MR"){
+            selected="selected";
+        }
+        let option =`<option value = "${country_code}"${selected}>${country[country_code]} </option>`;
+        tag.insertAdjacentHTML("beforeend", option);
+    }
+});
+
+exchageIcon.addEventListener("click", () => {
+    let tempText = fromText.value,
+    tempLang = selectTag[0].value;
+    fromText.value = toText.value;
+    selectTag[0].value = selectTag[1].value;
+    toText.value = tempText;
+    selectTag[1].value = tempLang;
+});
+translateBtn.addEventListener("click", () => {
+    let text = fromText.value,
+    translateFrom = selectTag[0].value,
+    translateTo = selectTag[1].value;
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+    fetch(apiUrl).then(res => res.json()).then(data => {
+        toText.value = data.responseData.translatedText;
+    });
+});
+
+icons.forEach(icon => {
+    icon.addEventListener("click", ({target}) => {
+        if(target.classList.contains("fa-copy")) {
+            if(target.id == "from") {
+                navigator.clipboard.writeText(fromText.value);
+            } else {
+                navigator.clipboard.writeText(toText.value);
+            }
+        } else {
+            let utterance;
+            if(target.id == "from") {
+                utterance = new SpeechSynthesisUtterance(fromText.value);
+                utterance.lang = selectTag[0].value;
+            } else {
+                utterance = new SpeechSynthesisUtterance(toText.value);
+                utterance.lang = selectTag[1].value;
+            }
+            speechSynthesis.speak(utterance);
+        }
+    });
+});
